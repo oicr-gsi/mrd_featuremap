eb2bb8b8fc919fc845f8a227c28414c6b1f02648
pruzanov <pruzanov@oicr.on.ca>
2026-09-10 11:59:46 -0400

    Small change to calculate.sh script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 cd $1
 
-echo "Count of PASS variants from the output vcfi files"
+echo "Count of PASS variants from the output vcf files"
 for v in *vcf.gz;do zcat $v | grep -v ^# | awk '$7=="PASS"' | wc -l;done
